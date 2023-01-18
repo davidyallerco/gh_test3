937fab5082d1105c0c4ff3e24a9a649bd3ec5c4e
davidyallerco <david.yallerco@gmail.com>
2023-01-18 05:46:32 -0500

    v1 inicial con entidades

---
+++ src/main/java/pe/parnertdigital/test3/exceptions/DineroInsuficienteException.java
@@ -0,0 +1,7 @@
+package pe.parnertdigital.test3.exceptions;
+
+public class DineroInsuficienteException extends RuntimeException{
+    public DineroInsuficienteException(String message) {
+        super(message);
+    }
+}
