6143966a32c2a4a014ff80c3a81490485e80f447
davidyallerco <david.yallerco@gmail.com>
2023-01-18 08:17:56 -0500

    v3 implementando test inconcluso

---
+++ src/main/java/pe/parnertdigital/test3/Datos.java
@@ -0,0 +1,12 @@
+package pe.parnertdigital.test3;
+
+import pe.parnertdigital.test3.models.Banco;
+import pe.parnertdigital.test3.models.Cuenta;
+
+import java.math.BigDecimal;
+
+public class Datos {
+    public static final Cuenta CUENTA_001 = new Cuenta(1L, "David", new BigDecimal("1000"));
+    public static final Cuenta CUENTA_002 = new Cuenta(2L, "Ezer", new BigDecimal("2000"));
+    public static final Banco BANCO = new Banco(1L, "El banco financiero", 0);
+}
--- src/test/java/pe/parnertdigital/test3/Test3ApplicationTests.java
+++ src/test/java/pe/parnertdigital/test3/Test3ApplicationTests.java
@@ -1,13 +1,43 @@
 package pe.parnertdigital.test3;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
+import pe.parnertdigital.test3.repositories.BancoRepository;
+import pe.parnertdigital.test3.repositories.CuentaRespository;
+import pe.parnertdigital.test3.services.CuentaService;
+import pe.parnertdigital.test3.services.CuentaServiceImpl;
+
+import java.math.BigDecimal;
+
+import static  org.mockito.Mockito.*;
 
 @SpringBootTest
 class Test3ApplicationTests {
 
+	CuentaRespository cuentaRespository;
+	BancoRepository bancoRepository;
+
+	CuentaService service;
+
+	@BeforeEach
+	void setUp() {
+		cuentaRespository = mock(CuentaRespository.class);
+		bancoRepository = mock(BancoRepository.class);
+		service = new CuentaServiceImpl(cuentaRespository, bancoRepository);
+	}
+
 	@Test
 	void contextLoads() {
+		when(cuentaRespository.buscarPorId(1L)).thenReturn(Datos.CUENTA_001);
+		when(cuentaRespository.buscarPorId(2L)).thenReturn(Datos.CUENTA_002);
+		when(bancoRepository.buscarPorId(1L)).thenReturn(Datos.BANCO);
+
+		//antes de realizar la transferencia
+		BigDecimal saldoOrigen = service.revisarSaldo(1L);
+		BigDecimal saldoDestino = service.revisarSaldo(2L);
+		
 	}
 
 }
